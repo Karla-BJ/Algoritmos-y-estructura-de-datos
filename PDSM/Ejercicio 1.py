3f8799b774e502c8f542dcdac6bbba141703afb5
Karla-BJ <karlaberriojalil7@gmail.com>
2021-07-29 11:55:06 -0500

    ,,

---
+++ PDSM/Ejercicio 1.py
@@ -0,0 +1,17 @@
+#Escriba un código en Python que calcule el área de una circunferencia a partir del radio preguntado al usuario#
+
+print ("Primer punto")
+r= float (input ("Ingrese el radio de la circunferencia a la cual desea calcular el área : "))
+area= 3.1416 * r**2
+
+print (f"el área de la circunferencia es {area})
+
+#if r != 0:
+#    print (area)
+#        else:
+#            print ("El área es 0")
+
+
+print ("Segundo punto")
+
+
